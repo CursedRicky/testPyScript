8b5e6d55aa4a6fe932727e840169555e2b496178
CursedRicky <78096957+CursedRicky@users.noreply.github.com>
2024-03-01 23:18:46 +0100

    test 1

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 from pyscript import document
 import random as r
 
-enable = False
-
-def test(*args, **kwargs):
-    print("A")
+def tiradado(valore):
     out = document.querySelector("#nascosto")
-    out.innerText = f"Valore {r.randint(1,6)}"
+    out.innerText = f"Valore: {r.randint(1,valore)}"
+
+def d6(*args, **kwargs):
+    tiradado(6)
+
+def d20(*args, **kwargs):
+    tiradado(20)
+
+def d2(*args, **kwargs):
+    tiradado(2)
+
+def d4(*args, **kwargs):
+    tiradado(4)
+
+def d8(*args, **kwargs):
+    tiradado(8)
+
+def d10(*args, **kwargs):
+    tiradado(10)
+
+def d12(*args, **kwargs):
+    tiradado(12)
+
+def d100(*args, **kwargs):
+    tiradado(100)
